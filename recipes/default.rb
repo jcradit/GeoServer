5868644b5c1cd866272db0ebd86db52ce8e5cad3
jcradit <jasoncradit@gmail.com>
2016-03-04 13:47:58 -0700

    no file

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,11 +1,11 @@
-file '/etc/yum.repos.d/OpenGeo.repo' do
-  content '[opengeo]
-  name=opengeo
-  baseurl=http://yum.opengeo.org/suite/v4/rhel/6/$basearch
-  enabled=1
-  gpgcheck=0'
-  mode '0755'
-end
+#file '/etc/yum.repos.d/OpenGeo.repo' do
+#  content '[opengeo]
+#  name=opengeo
+#  baseurl=http://yum.opengeo.org/suite/v4/rhel/6/$basearch
+#  enabled=1
+#  gpgcheck=0'
+#  mode '0755'
+#end
 
 package 'opengeo-server' do
   action [:install]
